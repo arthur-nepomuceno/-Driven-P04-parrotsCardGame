ae1561b904f94e22db5b998e379cfe8bb2ae2b39
= <=>
2022-04-06 20:08:44 -0300

    [Driven][P04]parrotsCardGame_0.3_cards_shuffling_OK

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-// requesting number of card to play
+// requesting number of cards to play
 // it must be an even number between 4 and 14
 let numberOfCards = 1
 while (numberOfCards % 2 != 0 || numberOfCards < 4 || numberOfCards > 14) {
+    alert("Por favor, escolha um NÚMERO PAR entre 4 e 14.")
     numberOfCards = Number(prompt("Com quantas cartas quer jogar?"));
 }
 
-// bringing card html inside javaScript code
-let card =  `<div class="card" onclick="flipCard(this)">
-                <div class="face back-face">
-                    <img src="/img/back.png" alt="">
-                </div>
-                <div class="face front-face">
-                    <img src="/img/revertitparrot.gif" alt="" srcset="">
-                </div>
-            </div>`
 
-// getting .deck <div>
-// and writting cards inside of it accordingly to user's choice
-// the idea is to use "for", laying down as many cards as choosen
-const deck = document.querySelector(".deck");
-for (let n = 0; n < numberOfCards; n++) {
-    deck.innerHTML += card;
+
+// bringing the list of images to work with
+let listOfImages = [
+    "bobrossparrot.gif","explodyparrot.gif",
+    "fiestaparrot.gif","metalparrot.gif",
+    "revertitparrot.gif","tripletsparrot.gif",
+    "unicornparrot.gif"]
+
+// creating the deck of images
+function deckOfImages(n) {
+    let deckOfImages = []
+    for (let i = 0; i < n/2; i++) {
+        deckOfImages.push(listOfImages[i]);
+        deckOfImages.push(listOfImages[i]);
+    }
+    // shufflig the deck
+    return deckOfImages;
 }
 
+// function to shuffle he deck
+function comparer() { 
+    return Math.random() - 0.5; 
+}
+
+let myDeckOfImages = deckOfImages(numberOfCards).sort(comparer);
+
+
+// creating the display of the cards on the screen
+const deck = document.querySelector(".deck");
+for (let n = 0; n < numberOfCards; n++) {
+    // bringing card html inside javaScript code
+    let card =  `<div class="card" onclick="flipCard(this)">
+                    <div class="face back-face">
+                        <img src="/img/back.png" alt="">
+                    </div>
+                    <div class="face front-face">
+                        <img src="/img/${myDeckOfImages[n]}" alt="" srcset="">
+                    </div>
+                </div>`
+
+    deck.innerHTML += card;
+}
 
 function flipCard(card) {
     let backFace = card.querySelector(".back-face")
