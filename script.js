12b7c452afd979e7c6abfe4a681c2c043acfe6e3
arthur-nepomuceno <arth.nepomuceno@gmail.com>
2022-04-06 15:16:01 -0300

    [Driven][P04]parrotsCardGame_0.2_cards_flipping_on_click_OK

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // requesting number of card to play
 // it must be an even number between 4 and 14
-let numberOfCards = 3
+let numberOfCards = 1
 while (numberOfCards % 2 != 0 || numberOfCards < 4 || numberOfCards > 14) {
     numberOfCards = Number(prompt("Com quantas cartas quer jogar?"));
 }
 
 // bringing card html inside javaScript code
-let card =  `<div class="card">
+let card =  `<div class="card" onclick="flipCard(this)">
                 <div class="face back-face">
                     <img src="/img/back.png" alt="">
+                </div>
+                <div class="face front-face">
+                    <img src="/img/revertitparrot.gif" alt="" srcset="">
                 </div>
             </div>`
 
@@ -18,4 +21,13 @@ let card =  `<div class="card">
 const deck = document.querySelector(".deck");
 for (let n = 0; n < numberOfCards; n++) {
     deck.innerHTML += card;
+}
+
+
+function flipCard(card) {
+    let backFace = card.querySelector(".back-face")
+    let frontFace = card.querySelector(".front-face")
+
+    backFace.setAttribute("style", "transform: rotateY(180deg)");
+    frontFace.setAttribute("style", "transform: rotate(0deg);")
 }
